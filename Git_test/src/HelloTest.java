5204473bc65adf7611a703ac89a958c4242f1660
John C. Earls <yt.rabb@gmail.com>
2012-06-19 17:07:40 -0700

    K lets see how this goes

---
+++ Git_test/src/HelloTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author earls3
+ *
+ */
+public class HelloTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("You suck");
+
+	}
+
+}
